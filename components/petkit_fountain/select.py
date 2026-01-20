279cc0deba5b88a62f2b5eb70778b37bfbf54a3c
Tobias Göbel <kubax1983@gmail.com>
2026-01-20 17:34:31 +0100

    fix select.py

--- components/petkit_fountain/select.py
+++ components/petkit_fountain/select.py
@@ -13,7 +13,7 @@ PetkitModeSelect = petkit_ns.class_("PetkitModeSelect", select.Select)
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.Required(CONF_PARENT_ID): cv.use_id(PetkitFountain),
-        cv.Optional(CONF_MODE_SELECT): select.select_schema(PetkitModeSelect, options=["normal", "smart"]),
+        cv.Optional(CONF_MODE_SELECT): select.select_schema(PetkitModeSelect),
     }
 )
 
@@ -21,5 +21,6 @@ async def to_code(config):
     parent = await cg.get_variable(config[CONF_PARENT_ID])
 
     if CONF_MODE_SELECT in config:
-        s = await select.new_select(config[CONF_MODE_SELECT], options=["normal", "smart"])
-        cg.add(s.set_parent(parent))
+        # Options are defined here (works across versions)
+        sel = await select.new_select(config[CONF_MODE_SELECT], options=["normal", "smart"])
+        cg.add(sel.set_parent(parent))
