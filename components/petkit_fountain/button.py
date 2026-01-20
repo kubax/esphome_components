918ae82fa36f61318ce0fad30b51dfaf9b60ebbf
Tobias Göbel <kubax1983@gmail.com>
2026-01-20 17:36:22 +0100

    fix button.py

--- components/petkit_fountain/button.py
+++ components/petkit_fountain/button.py
@@ -1,7 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import button
-from esphome.const import CONF_ID
 
 CONF_PARENT_ID = "parent_id"
 
@@ -30,23 +29,28 @@ CONFIG_SCHEMA = cv.Schema(
 async def to_code(config):
     parent = await cg.get_variable(config[CONF_PARENT_ID])
 
-    def _mk(key, action_name):
-        if key in config:
-            b = await button.new_button(config[key])
-            cg.add(b.set_parent(parent))
-            # action enum set via integer mapping, keeps it simple
-            # REFRESH=0 RESET_FILTER=1 SET_DATETIME=2 INIT_SESSION=3 SYNC=4
-            mapping = {
-                "REFRESH": 0,
-                "RESET_FILTER": 1,
-                "SET_DATETIME": 2,
-                "INIT_SESSION": 3,
-                "SYNC": 4,
-            }
-            cg.add(b.set_action(mapping[action_name]))
+    # Action mapping: REFRESH=0 RESET_FILTER=1 SET_DATETIME=2 INIT_SESSION=3 SYNC=4
+    if CONF_REFRESH_STATE in config:
+        b = await button.new_button(config[CONF_REFRESH_STATE])
+        cg.add(b.set_parent(parent))
+        cg.add(b.set_action(0))
 
-    _mk(CONF_REFRESH_STATE, "REFRESH")
-    _mk(CONF_RESET_FILTER, "RESET_FILTER")
-    _mk(CONF_SET_DATETIME, "SET_DATETIME")
-    _mk(CONF_INIT_SESSION, "INIT_SESSION")
-    _mk(CONF_SYNC, "SYNC")
+    if CONF_RESET_FILTER in config:
+        b = await button.new_button(config[CONF_RESET_FILTER])
+        cg.add(b.set_parent(parent))
+        cg.add(b.set_action(1))
+
+    if CONF_SET_DATETIME in config:
+        b = await button.new_button(config[CONF_SET_DATETIME])
+        cg.add(b.set_parent(parent))
+        cg.add(b.set_action(2))
+
+    if CONF_INIT_SESSION in config:
+        b = await button.new_button(config[CONF_INIT_SESSION])
+        cg.add(b.set_parent(parent))
+        cg.add(b.set_action(3))
+
+    if CONF_SYNC in config:
+        b = await button.new_button(config[CONF_SYNC])
+        cg.add(b.set_parent(parent))
+        cg.add(b.set_action(4))
